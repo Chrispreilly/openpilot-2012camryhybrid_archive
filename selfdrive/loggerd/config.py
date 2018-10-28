fcc08e89829a54b6465b3b3d3543aecab5c150bd
camryh <cpreilly@buffalo.edu>
2018-10-27 22:18:44 -0400

    back to standard upload

--- selfdrive/loggerd/config.py
+++ selfdrive/loggerd/config.py
@@ -4,6 +4,6 @@ if os.environ.get('LOGGERD_ROOT', False):
   ROOT = os.environ['LOGGERD_ROOT']
   print("Custom loggerd root: ", ROOT)
 else:
-  ROOT = '/data/media/0/realdataupload/'
+  ROOT = '/data/media/0/realdata/'
 
 SEGMENT_LENGTH = 60
