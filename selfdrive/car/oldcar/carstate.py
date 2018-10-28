397bbbc222bb14734f519c2fcc0330d8feebe5d1
camryh <cpreilly@buffalo.edu>
2018-10-28 17:53:56 -0400

    Update carstate.py

--- selfdrive/car/oldcar/carstate.py
+++ selfdrive/car/oldcar/carstate.py
@@ -143,7 +143,7 @@ class CarState(object):
     
     self.cruise_stalk_pull = cp.vl["PCM_CRUISE_SM"]['MAIN_ON'] == 1
  
-    self.brake_pressed = cp.vl["BRAKE_MODULE"]['BRAKE_PRESSED']
+    self.brake_pressed = cp.vl["BRAKE_MODULE"]['BRAKE_PRESSED'] > 0
       
       
       #Below values never update
