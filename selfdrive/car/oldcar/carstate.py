148f38d57aea707ff5be7d7ff246d6fe444f6d39
Chrispreilly <cpreilly@buffalo.edu>
2018-11-15 10:31:15 -0500

    decrease wait before start time

--- selfdrive/car/oldcar/carstate.py
+++ selfdrive/car/oldcar/carstate.py
@@ -114,7 +114,7 @@ class CarState(object):
     self.CL_TIMEA_BP = [10., 32., 44.]
     self.CL_TIMEA_T = [1.2 ,0.80, 0.70] #[0.7 ,0.30, 0.20]
     #duration to wait (in seconds) with blinkers on before starting to turn
-    self.CL_WAIT_BEFORE_START = 1
+    self.CL_WAIT_BEFORE_START = 0.5 #1
     #END OF ALCA PARAMS
    
     
