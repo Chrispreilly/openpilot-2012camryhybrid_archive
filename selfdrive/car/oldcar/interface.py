4465bd31099067e9e40ebc662ede660b21477282
Chrispreilly <cpreilly@buffalo.edu>
2018-11-17 23:34:52 -0500

    update with good parameters

--- selfdrive/car/oldcar/interface.py
+++ selfdrive/car/oldcar/interface.py
@@ -79,8 +79,8 @@ class CarInterface(object):
     tireStiffnessFront_civic = 192150
     tireStiffnessRear_civic = 202500
 
-    ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
-    ret.steerActuatorDelay = 0.12 #12  # Default delay, Prius has larger delay
+ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
+    ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
 
     if candidate == CAR.COROLLA:
       ret.safetyParam = 100 # see conversion factor for STEER_TORQUE_EPS in dbc file
