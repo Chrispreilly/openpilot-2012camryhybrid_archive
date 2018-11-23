ab2b460e68a1366cb94f5c263a6c24dabcfead05
Chrispreilly <cpreilly@buffalo.edu>
2018-11-23 12:27:15 -0500

    typo

--- selfdrive/car/oldcar/carcontroller.py
+++ selfdrive/car/oldcar/carcontroller.py
@@ -164,7 +164,7 @@ class CarController(object):
     # accel cmd comes from DSU, but we can spam can to cancel the system even if we are using lat only control
     if (frame % 3 == 0 and ECU.DSU in self.fake_ecus) or (pcm_cancel_cmd and ECU.CAM in self.fake_ecus):
       if ECU.DSU in self.fake_ecus:
-        can_sends.append(create_accel_cgommand(self.packer, apply_accel, pcm_cancel_cmd, self.standstill_req))
+        can_sends.append(create_accel_command(self.packer, apply_accel, pcm_cancel_cmd, self.standstill_req))
       else:
         can_sends.append(create_accel_command(self.packer, 0, pcm_cancel_cmd, False))
 
