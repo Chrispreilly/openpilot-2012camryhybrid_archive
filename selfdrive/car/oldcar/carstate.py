7b5a993f4e2abcb662795c2c7ee9d666db494509
Chrispreilly <cpreilly@buffalo.edu>
2019-01-06 19:54:46 -0500

    Update carstate.py

--- selfdrive/car/oldcar/carstate.py
+++ selfdrive/car/oldcar/carstate.py
@@ -75,9 +75,9 @@ class CarState(object):
     self.btns_init = [["","",[""]], \
                       ["alca","ALC",["MadMax","Normal","Wifey"]], \
                       ["sound","SND",[""]], \
-                      ["accengage","ACC",[""]], \
-                      ["upfive", "+5",[""]], \
-                      ["downfive", "-5", [""]]]
+                      ["accengage","ACC",["0","1"]], \
+                      ["upfive", "+5",["0","1"]], \
+                      ["downfive", "-5", ["0","1"]]]
     #if (CP.carFingerprint == CAR.MODELS):
     # ALCA PARAMS
     # max REAL delta angle for correction vs actuator
